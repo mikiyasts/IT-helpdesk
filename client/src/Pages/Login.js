a61786d0a516160edc890b3af14a3d2c1be1b646
wanofi-israel <wanofiisrael17@gmail.com>
2024-09-19 09:19:02 +0300

    login api

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,87 +1,100 @@
 import React, { useState } from "react";
 import Logo from "../asset/Image/logo.png"
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-
+import axios from "axios"
 function Login() {
   const [tab, setTab] = useState("signin");
-  const [forgotPopup,setforgotPopup]=useState(false)
-
-  
-
+  const [forgotPopup, setforgotPopup] = useState(false)
+  const [loginform, Setloginform] = useState({ username: "", password: "" })
 
 
-  const submitLogin=(e)=>{
-    e.preventdefault()
+
+
+  const submitLogin = (e) => {
+    e.preventDefault()
+    axios.post("http://localhost:8000/api/login", loginform)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
     console.log("login");
-    
   }
+  const handleLoginchange = (e) => {
+    Setloginform(prev => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  console.log(loginform)
+
   return (
     <div className="login-page">
       {/* popup*/}
-      <div className={`forgot-password_popup ${forgotPopup &&"active"}`}>
-        <div className="popup-pin" onClick={()=>{
-    setforgotPopup(false)
-  }}></div>
-       
+      <div className={`forgot-password_popup ${forgotPopup && "active"}`}>
+        <div className="popup-pin" onClick={() => {
+          setforgotPopup(false)
+        }}></div>
+
         <div className="popup_header"><h2>Reset your Password</h2></div>
 
         <form className="reset-form">
           <div className="form-ctrl">
             <label htmlFor="reset-email">Email Address <span>*</span></label>
-            <input type="email" name="email" id="reset-email"/>
+            <input type="email" name="email" id="reset-email" />
           </div>
           <div className="forgot-signin sigup-btn-conatiner">
-                <button className="btn-login">Send Reset Link</button>
-              </div>
+            <button className="btn-login">Send Reset Link</button>
+          </div>
         </form>
       </div>
       {/* popup*/}
       <div className="login-left">
         <div className="tab-container">
           <div className="header-container">
-          <div className="logo"><img src={Logo} alt='Logo' width="45px"/></div>
-          <div className="app-name"><h2>Awash Wine IT Help Desk</h2></div>
+            <div className="logo"><img src={Logo} alt='Logo' width="45px" /></div>
+            <div className="app-name"><h2>Awash Wine IT Help Desk</h2></div>
           </div>
           {/* signin container */}
-          <div className={`login-container ${tab==="signin"&&"active-signin"}`}>
-            <form className="login-form" onSubmit={submitLogin}>
+          <div className={`login-container ${tab === "signin" && "active-signin"}`}>
+            <form className="login-form" onSubmit={submitLogin} onChange={handleLoginchange}>
               <div className="login-header">
                 <h1>Welcome Back !</h1>
               </div>
               <div className="form-ctrl">
                 <label htmlFor="id">Email Address</label>
-                <input type="text" name="emp_id" id="id" />
+                <input type="text" name="username" id="id" />
               </div>
               <div className="form-ctrl">
                 <label htmlFor="password">Password</label>
                 <input type="password" name="password" id="password" />
               </div>
               <div className="forgot-signin">
-                <p onClick={()=>{
-    setforgotPopup(true)
-  }}>Forgot Password ?</p>
+                <p onClick={() => {
+                  setforgotPopup(true)
+                }}>Forgot Password ?</p>
                 <button className="btn-login" >Sign In</button>
               </div>
             </form>
             <div className="signup">
               <p>
-                Don't you have an account? 
-              </p> <span className="tab" onClick={()=>{
-                  setTab("signup")
-                }}> Sign Up</span>
+                Don't you have an account?
+              </p> <span className="tab" onClick={() => {
+                setTab("signup")
+              }}> Sign Up</span>
             </div>
           </div>
           {/* signin container */}
 
           {/* signup container */}
-          <div className={`signup-container ${tab==="signup"&&"active-signup"}`}>
+          <div className={`signup-container ${tab === "signup" && "active-signup"}`}>
             <form className="login-form">
               <div className="login-header">
                 <h1 className="create-account">Create you account</h1>
               </div>
               <div className="form-ctrl">
                 <label htmlFor="id">Email Address</label>
-                <input type="text" name="emp_id" id="id" />
+                <input type="text" name="email" id="sid" />
               </div>
               <div className="form-ctrl">
                 <label htmlFor="fullname">Full Name</label>
@@ -116,10 +129,10 @@ function Login() {
             </form>
             <div className="signup">
               <p>
-                Have an account? 
-              </p><span className="tab" onClick={()=>{
-                  setTab("signin")
-                }}> Sign In</span>
+                Have an account?
+              </p><span className="tab" onClick={() => {
+                setTab("signin")
+              }}> Sign In</span>
             </div>
           </div>
           {/* signup container */}
