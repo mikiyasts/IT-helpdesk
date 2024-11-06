1331fbe1d6d8d61294ad8c429d97ef8296f30658
wanofi-israel <wanofiisrael17@gmail.com>
2024-11-06 08:48:33 +0300

    active date preview working

--- client/src/Pages/admin/Tickets.js
+++ client/src/Pages/admin/Tickets.js
@@ -118,6 +118,12 @@ function Tickets() {
       }
     })
   }
+  let activeDate
+  const activeTicketdate=()=>{
+    activeDate=activePreview?.created_at.split("T")[0]    
+  }
+
+  activeTicketdate()
   return (
     <>
     <div className="filter-div">
@@ -199,7 +205,7 @@ function Tickets() {
             <h4>Details</h4> 
            { detailcollapse? <ExpandLessIcon/>: <ExpandMoreIcon sx={{ fontSize: 20 }}/>}
           <ul className={`detail-list ${detailcollapse&&"active"}`}>
-            <li><div className="detail-name">Request Time</div> <div className="detail-value">{ ()=>{return hello"}}</div></li>
+            <li><div className="detail-name">Request Time</div> <div className="detail-value">{activeDate}</div></li>
             <li><div className="detail-name">Location</div> <div className="detail-value">{activePreview && activePreview.created_by && activePreview.created_by.branch}</div></li>
             <li><div className="detail-name">Title</div> <div className="detail-value"><p>{activePreview && activePreview.title}</p></div></li>
             <li><div className="detail-name">Note</div> <div className="detail-value"><p>Note</p></div></li>
